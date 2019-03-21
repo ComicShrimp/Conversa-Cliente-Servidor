18367270a5e2e480cf5d89c8b4cfbdcd9be9bb1c
Mário Victor Ribeiro Silva <mario.victor@unifesspa.edu.br>
2019-03-20 22:26:29 -0300

    Parte do Cliente está completa e comentada.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Conversa-Cliente-Servidor
+# Conversa Cliente Servidor
 Script em python para realizar uma troca de mensagens entre um cliente e um servidor
--- Server.py
+++ Server.py
@@ -4,3 +4,22 @@ import socket
 
 server_ip = '127.0.0.1'
 porta = 2024
+
+# A linha abixo inicializa o socket com o protocolo ipv4(AF_INET) e tcp(SOCK_STREAM).
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((server_ip, porta))
+
+    # No listen também pode ser passado o parâmetro de numero de conexões simultaneas.
+    # Faz com que a porta passe a ser de escuta, e o permite aceitar conexões(Accept logo abaixo).
+    s.listen()
+
+    # Accept "Trava" o programa até que ele receba uma conexão.
+    # O socket que é usado para poder escutar, é diferente do socket de comunicação, por isso o conn
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
