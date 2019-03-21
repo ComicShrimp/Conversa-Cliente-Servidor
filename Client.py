d167be3a7153e35681ff1beaca73043e7c898e09
Mário Victor Ribeiro Silva <mario.victor@unifesspa.edu.br>
2019-03-21 10:49:36 -0300

    Agora o servidor fica aberto até que o programa seja encerrado, e o cliente está completamente funcional.

--- Client.py
+++ Client.py
@@ -7,7 +7,8 @@ porta = 2024
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((server_ip, porta))
-    s.sendall(b'Ola Mundo')
+    msg = input('Digita a mensagem: ')
+    s.sendall(msg.encode())
     data = s.recv(1024)
 
-print('Recebido', repr(data))
+print('Recebido', repr(data.decode()))
--- Server.py
+++ Server.py
@@ -8,6 +8,7 @@ porta = 2024
 # A linha abixo inicializa o socket com o protocolo ipv4(AF_INET) e tcp(SOCK_STREAM).
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((server_ip, porta))
+    print('Servidor Iniciado')
 
     # No listen também pode ser passado o parâmetro de numero de conexões simultaneas.
     # Faz com que a porta passe a ser de escuta, e o permite aceitar conexões(Accept logo abaixo).
@@ -15,8 +16,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
     # Accept "Trava" o programa até que ele receba uma conexão.
     # O socket que é usado para poder escutar, é diferente do socket de comunicação, por isso o conn
-    conn, addr = s.accept()
-    with conn:
+    while True:
+        conn, addr = s.accept()
         print('Connected by', addr)
         while True:
             data = conn.recv(1024)
