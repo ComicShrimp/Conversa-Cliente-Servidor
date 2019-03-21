ce470afc9526df42fc921ebd5a7181cc55ee80cb
Mário Victor Ribeiro Silva <mario.victor@unifesspa.edu.br>
2019-03-21 14:51:49 -0300

    Agora o codigo cliente fica se repetindo.

--- Client.py
+++ Client.py
@@ -5,10 +5,11 @@ import socket
 server_ip = '127.0.0.1'
 porta = 2024
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((server_ip, porta))
-    msg = input('Digita a mensagem: ')
-    s.sendall(msg.encode())
-    data = s.recv(1024)
+while True:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((server_ip, porta))
+        msg = input('Digita a mensagem: ')
+        s.sendall(msg.encode())
+        data = s.recv(1024)
 
-print(repr(data.decode()))
+    print(repr(data.decode()))
