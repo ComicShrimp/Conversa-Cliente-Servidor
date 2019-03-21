a404f8b162c343e248740c080a07e5ad4c867efe
Mário Victor Ribeiro Silva <mario.victor@unifesspa.edu.br>
2019-03-21 10:17:45 -0300

    Parte de cliente completa e o codigo está funcionando

--- Client.py
+++ Client.py
@@ -4,3 +4,10 @@ import socket
 
 server_ip = '127.0.0.1'
 porta = 2024
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((server_ip, porta))
+    s.sendall(b'Ola Mundo')
+    data = s.recv(1024)
+
+print('Recebido', repr(data))
